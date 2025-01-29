6ae21ea3658b4d465b8119df636fdf8d6d7080d3
Ana Sánchez Muñoz <ana.sanmua@gmail.com>
2025-01-29 18:48:40 +0100

    removed evidences of critters

--- next.config.mjs
+++ next.config.mjs
@@ -3,10 +3,6 @@ import createNextIntlPlugin from "next-intl/plugin";
 const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    optimizeCss: true, // Enables built-in CSS optimization
-  },
-};
+const nextConfig = {};
 
 export default withNextIntl(nextConfig);
